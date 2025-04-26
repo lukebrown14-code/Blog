17f418df5e77cb8b44f35efb9bcd2bc1ecc2fb69
Luke B <lukebrown14@hotmail.com>
2025-04-26 09:18:48 +0100

    Installed Svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<div class="p-8 text-left">
+	<h1 class="text-6xl decoration-dotted"><a href="/">Archiplego</a></h1>
+</div>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,1 @@
+<p><a href="/about">About</a></p>
