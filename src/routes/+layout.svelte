1f57c922759d082065b9a53a629144ddb7294e96
Luke B <lukebrown14@hotmail.com>
2025-04-27 09:23:53 +0100

    Create the title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,8 +4,15 @@
 	let { children } = $props();
 </script>
 
-<div class="p-8 text-left">
-	<h1 class="text-6xl decoration-dotted"><a href="/">Archiplego</a></h1>
+<div class="ml-15 relative mt-5 flex flex-col items-start justify-start">
+	<div class="text-left text-stone-900">
+		<h1 id="title" class="text-6xl title-hover">
+			<a href="/">ar·chi·pel·ago</a>
+		</h1>
+		<h2 id="about" class="text-lg title-hover italic font-light text-stone-700 opacity-70">
+			<a href="/about">By Luke Brown</a>
+		</h2>
+	</div>
 </div>
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,2 @@
-<p><a href="/about">About</a></p>
+<h1 class="pt-16">POSTS</h1>
+<p><a href="/blog/hello-world">Hello world Blog</a></p>
